e4ab3f54501f60432630b35a4c69c9181fc76442
Pratap Subrahmanyam <pratapsu@gmail.com>
2025-01-01 09:25:34 -0800

    Install the scripts into the vault.

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cp exerciseStats.js goodReadings.js linkstoContacts.js pressureHistogram.js ~/Documents/Story\ of\ Me/Other\ Scripts
+cp randomQuote.js quotes.json ~/Documents/Story\ of\ Me/Templater\ Scripts
