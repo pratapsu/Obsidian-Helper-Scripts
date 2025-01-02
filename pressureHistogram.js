b679459d24ea5837110293499be67722818a051a
Pratap Subrahmanyam <pratapsu@gmail.com>
2025-01-01 18:35:14 -0800

    Only show the histogram until currenmt week (not for the entire duration of the Upavasa).

--- pressureHistogram.js
+++ pressureHistogram.js
@@ -1,11 +1,12 @@
 // Takes a week number as input and returns a hsitorgram (an array of intergers)
+const { DateTime } = dv.luxon
+const begin = DateTime.fromString("Nov 1, 2024", "LLL d, yyyy")
 function getWeeklyHistogram(wkNum) {
-	const { DateTime } = dv.luxon
 	const offset = 7*(wkNum - 1)
-	const begin = DateTime.fromString("Nov 1, 2024", "LLL d, yyyy").plus({days: offset})
-	const end = begin.plus({days: 7});
+	const beginInWeek = begin.plus({days: offset})
+	const endInWeek = beginInWeek.plus({days: 7});
 	let p = dv.pages('"Health"')
-		.filter(q => ((begin <= q.file.ctime) && (q.file.ctime <= end)))
+		.filter(q => ((beginInWeek <= q.file.ctime) && (q.file.ctime <= endInWeek)))
 
 	let buckets = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
 	p.forEach((page) => {
@@ -39,9 +40,14 @@ function row(i) {
 }
 
 function drawHistogram() {
+	let nWeeks = Math.ceil(DateTime.now().diff(begin, 'days').days / 7)
+	let weeks = []
+	for (let i = 0; i < nWeeks; i++) {
+		weeks.push(i + 1)
+	}
     dv.table(
         ["Week #", "< 110", "< 115", "< 120", "< 125", "< 130", "< 135", "< 140", "< 145", "< 150", "< 155", "< 160", "Gt 160"], 
-   		[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map((i) => row(i))
+		weeks.map((i) => row(i))
 	)
 }
 
